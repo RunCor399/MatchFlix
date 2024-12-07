b0d84e246d63fb31a7c43d5f607fb2e589bbc1c9
Runcor3 <manuel.colotti@gmail.com>
2024-12-07 12:58:25 +0100

    Conflict

--- testhmac.py
+++ testhmac.py
@@ -11,7 +11,6 @@ input_str = f"{username}:{realm}:{password}"
 hmac = hashlib.md5(input_str.encode()).hexdigest()
 
 print(hmac)
-<<<<<<< HEAD
 
 
 
@@ -40,5 +39,3 @@ print(hmac)
 # secret = "secretkey"
 # credentials = get_turn_credentials(name, secret)
 # print(credentials)
-=======
->>>>>>> cbf5f8905d326da358a2cabf1f3c35e18b2868f4
